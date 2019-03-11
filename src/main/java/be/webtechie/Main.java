9dc4dd0ab9187fc2da4a58a629b463a6621fb059
Frank Deporte <frank@webtechie.be>
2019-03-11 12:59:32 +0100

    Added more TilesFX examples in the test application

--- src/main/java/be/webtechie/Main.java
+++ src/main/java/be/webtechie/Main.java
@@ -2,11 +2,12 @@ package be.webtechie;
 
 import eu.hansolo.tilesfx.Tile;
 import eu.hansolo.tilesfx.TileBuilder;
+import eu.hansolo.tilesfx.tools.Location;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.util.Locale;
@@ -22,7 +23,7 @@ public class Main extends Application {
 
         Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
 
-        Tile clockTile = TileBuilder.create()
+        var clockTile = TileBuilder.create()
                 .skinType(Tile.SkinType.CLOCK)
                 .prefSize(200, 200)
                 .title("Clock Tile")
@@ -32,7 +33,27 @@ public class Main extends Application {
                 .running(true)
                 .build();
 
-        Scene scene = new Scene(new StackPane(clockTile), 640, 480);
+        var worldTile = TileBuilder.create()
+                .prefSize(200, 200)
+                .skinType(Tile.SkinType.WORLDMAP)
+                .title("WorldMap Tile")
+                .text("Whatever text")
+                .textVisible(false)
+                .build();
+
+        var mapTile = TileBuilder.create()
+                .skinType(Tile.SkinType.MAP)
+                .prefSize(500, 200)
+                .title("Map")
+                .text("Some text")
+                .description("Description")
+                .currentLocation(new Location(50.900028, 3.017390, "Home", Tile.TileColor.MAGENTA.color))
+                .pointsOfInterest(new Location(50.901098, 3.019505, "School", Tile.TileColor.RED.color),
+                        new Location(50.900131, 3.021212, "Shop", Tile.TileColor.BLUE.color))
+                .mapProvider(Tile.MapProvider.TOPO)
+                .build();
+
+        Scene scene = new Scene(new HBox(clockTile, worldTile, mapTile), 800, 480);
         stage.setScene(scene);
         stage.show();
     }
