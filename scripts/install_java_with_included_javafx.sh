6a8c27aa33584a41f5c3395ad4839cb1cda30182
Frank Delporte <frank@webtechie.be>
2019-04-03 22:47:34 +0200

    complete java version

--- scripts/install_java_with_included_javafx.sh
+++ scripts/install_java_with_included_javafx.sh
@@ -6,7 +6,7 @@
 # Make sure we are in the home directory
 cd /home/pi
 
-# Download the Java 11 distribution from BellSoft
+# Download the Java 11.0.2 distribution from BellSoft
 wget https://download.bell-sw.com/java/11.0.2/bellsoft-jdk11.0.2-linux-arm32-vfp-hflt.tar.gz
 
 # Move the downloaded file to /opt
